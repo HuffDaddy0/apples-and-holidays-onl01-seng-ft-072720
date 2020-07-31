ad39274076eb5548b258492cbf4ba39580a827b7
Tyler Huffman <ty.huff24@gmail.com>
2020-07-31 19:14:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
       holiday.each do |holiday_s , supplies|
-    puts "  #{holiday_s.capitalize}: #{supplies.join(supplies.last ? ", " : "")}"
+    puts "  #{holiday_s.capitalize.gsub("_"," ")}: #{supplies.join(supplies.last ? ", " : "")}"
   end
   end
   # iterate through holiday_hash and print items such that your readout resembles:
