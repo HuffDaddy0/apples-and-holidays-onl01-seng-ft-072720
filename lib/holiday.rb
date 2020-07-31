e2e9ea17f10624d5a81f297a216d66fee32f2798
Tyler Huffman <ty.huff24@gmail.com>
2020-07-31 20:01:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,5 +81,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+#  holidays_w_bbq = []
+holiday_hash.find_all do |season , holiday|
+  holiday.values.include?("BBQ")
+  holiday
+end
+  
 end
