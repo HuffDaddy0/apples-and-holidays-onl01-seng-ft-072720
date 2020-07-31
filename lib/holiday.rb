e5a8417183b717dcf3001ed454c958660e8d46a5
Tyler Huffman <ty.huff24@gmail.com>
2020-07-31 18:50:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,6 +55,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season
     puts "#{holiday}: #{holiday.values.flatten}"
+  end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
